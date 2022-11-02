ef45f66d4eb188b368e834289d7db406ca0b8781
Konstantin Mirsaitov <myrsaitov@gmail.com>
2022-11-02 16:20:15 +0300

    Scripts

--- __git_pull.cmd
+++ __git_pull.cmd
@@ -1,3 +1,48 @@
-git pull
+@echo off
+
+REM Выполняемая команда
+set GIT_CMD=git pull
+
+REM Нужно для цветных сообщений
+setlocal
+call :setESC
+cls
+
+REM Печатает путь к репозиторию
+echo %ESC%[44mRepository path:%ESC%[0m 
+echo %ESC%[33m%CD%%ESC%[0m
+
+REM Печатает команду, которая будет выполнена
+echo %ESC%[44mCommand:%ESC%[0m 
+echo %GIT_CMD%
+
+echo %ESC%[44mResult message from Git:%ESC%[0m
+%GIT_CMD% && (  REM Если результат выполнения удачный
+
+    REM Вывод сообщения "[ OK ]" зелёным
+    setlocal
+    call :setESC
+    echo;
+    echo %ESC%[32m[ OK ]%ESC%[0m
+
+)||(  REM Если результат выполнения неудачный
+    
+    REM Вывод сообщения "[ Error ]" красным
+    setlocal
+    call :setESC
+    echo;
+    echo %ESC%[31m[ Error ]%ESC%[0m
+
+)
+
+echo;
 
 pause
+
+REM Нужно для цветных сообщений
+:setESC
+for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1) do rem"') do (
+  set ESC=%%b
+  exit /B 0
+)
+exit /B 0
