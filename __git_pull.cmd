55ccd175918c543fd422eecd53e73b8291a95752
Konstantin Mirsaitov <myrsaitov@gmail.com>
2022-11-07 17:12:04 +0300

    Recursively Updated

--- __git_pull.cmd
+++ __git_pull.cmd
@@ -1,49 +1,13 @@
 @echo off
+REM Выполняет команду PULL для текущего репозитория
 
-REM Выполняемая команда
-set GIT_CMD=git pull
+REM Запоминает корневую папку
+set ROOT_PATH=%cd%
 
-REM Нужно для цветных сообщений
-setlocal
-call :setESC
-cls
+REM Переходит в рабочую папку
+cd ./../__Git.Scripts/__private
 
-REM Печатает путь к репозиторию
-echo %ESC%[44mPath:%ESC%[0m 
-echo %ESC%[33m%CD%%ESC%[0m
-
-REM Печатает команду, которая будет выполнена
-echo %ESC%[44mCommand:%ESC%[0m 
-echo %GIT_CMD%
-
-echo %ESC%[44mResult message from Git:%ESC%[0m
-%GIT_CMD% && (  REM Если результат выполнения удачный
-
-    REM Вывод сообщения "[ OK ]" зелёным
-    setlocal
-    call :setESC
-    echo;
-    echo %ESC%[32m[ OK ]%ESC%[0m
-
-)||(  REM Если результат выполнения неудачный
-    
-    REM Вывод сообщения "[ Error ]" красным
-    setlocal
-    call :setESC
-    echo;
-    echo %ESC%[31m[ Error ]%ESC%[0m
-
-)
-
-echo;
+REM Выполняет команду PULL
+call __git_CMD.cmd %ROOT_PATH% "git pull"
 
 pause
-
-REM Нужно для цветных сообщений
-goto:eof
-:setESC
-for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1) do rem"') do (
-  set ESC=%%b
-  exit /B 0
-)
-exit /B 0
--- __git_push.cmd
+++ __git_push.cmd
@@ -1,49 +1,14 @@
 @echo off
+REM Выполняет команду PUSH для текущего репозитория
 
-REM Выполняемая команда
-set GIT_CMD=git push -u origin master
 
-REM Нужно для цветных сообщений
-setlocal
-call :setESC
-cls
+REM Запоминает корневую папку
+set ROOT_PATH=%cd%
 
-REM Печатает путь к репозиторию
-echo %ESC%[44mPath:%ESC%[0m 
-echo %ESC%[33m%CD%%ESC%[0m
+REM Переходит в рабочую папку
+cd ./../__Git.Scripts/__private
 
-REM Печатает команду, которая будет выполнена
-echo %ESC%[44mCommand:%ESC%[0m 
-echo %GIT_CMD%
-
-echo %ESC%[44mResult message from Git:%ESC%[0m
-%GIT_CMD% && (  REM Если результат выполнения удачный
-
-    REM Вывод сообщения "[ OK ]" зелёным
-    setlocal
-    call :setESC
-    echo;
-    echo %ESC%[32m[ OK ]%ESC%[0m
-
-)||(  REM Если результат выполнения неудачный
-    
-    REM Вывод сообщения "[ Error ]" красным
-    setlocal
-    call :setESC
-    echo;
-    echo %ESC%[31m[ Error ]%ESC%[0m
-
-)
-
-echo;
+REM Выполняет команду PUSH
+call __git_CMD.cmd %ROOT_PATH% "git push -u origin master"
 
 pause
-
-REM Нужно для цветных сообщений
-goto:eof
-:setESC
-for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1) do rem"') do (
-  set ESC=%%b
-  exit /B 0
-)
-exit /B 0
