d3b37dbb0e04d9a9219d6b11eb5199615f7a26a8
Konstantin Myrsaitov <myrsaitov@gmail.com>
2022-11-09 13:27:07 +0300

    Recursively Updated

--- __git_push.cmd
+++ __git_push.cmd
@@ -8,7 +8,7 @@ REM Запоминает корневую папку
 set ROOT_PATH=%cd%
 
 REM Переходит в рабочую папку
-cd ./../__Git.Scripts/__private
+cd ./../__Git.Scripts.Windows/__private
 
 REM Выполняет команду PUSH
 call __git_CMD.cmd %ROOT_PATH% "git push -u origin master"
