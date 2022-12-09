e871a6a8528ef478a45505ac631bd36a3a8bc360
Konstantin Myrsaitov <myrsaitov@gmail.com>
2022-12-09 13:47:09 +0300

    Recursively Updated

--- __git_push.cmd
+++ __git_push.cmd
@@ -5,6 +5,6 @@
 TITLE Run "PUSH [%CD%]"
 
 :: Выполняет команду PUSH
-CALL ./../__Git.Scripts.Windows/__private/__git_CMD.cmd --PATH %CD% CALL ./../__Git.Scripts.Windows/__private/__git_check_unpushed_and_push.cmd %CD%
+CALL ./../__Git.Scripts.Windows/__private/__git_CMD.cmd --PATH %CD% CALL ./../__Git.Scripts.Windows/__private/__git_check_unpushed_and_push.cmd
 
 PAUSE
