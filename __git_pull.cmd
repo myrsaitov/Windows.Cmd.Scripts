540369222dd7b3b8af1c0850a5fbe129e42285f1
Konstantin Myrsaitov <myrsaitov@gmail.com>
2022-12-09 13:19:09 +0300

    Recursively Updated

--- __git_pull.cmd
+++ __git_pull.cmd
@@ -4,13 +4,7 @@
 :: Устанавливает заголовок окна
 TITLE Run "PULL [%CD%]"
 
-:: Запоминает корневую папку
-SET ROOT_PATH=%CD%
-
-:: Переходит в рабочую папку
-CD ./../__Git.Scripts.Windows/__private
-
 :: Выполняет команду PULL
-CALL __git_CMD.cmd --PATH %ROOT_PATH% git pull
+CALL ./../__Git.Scripts.Windows/__private/__git_CMD.cmd --PATH %CD% git pull
 
 PAUSE
--- __git_push.cmd
+++ __git_push.cmd
@@ -4,13 +4,7 @@
 :: Устанавливает заголовок окна
 TITLE Run "PUSH [%CD%]"
 
-:: Запоминает корневую папку
-SET ROOT_PATH=%CD%
-
-:: Переходит в рабочую папку
-CD ./../__Git.Scripts.Windows/__private
-
 :: Выполняет команду PUSH
-CALL __git_CMD.cmd --PATH %ROOT_PATH% CALL ./../__Git.Scripts.Windows/__private/__git_check_unpushed_and_push.cmd
+CALL ./../__Git.Scripts.Windows/__private/__git_CMD.cmd --PATH %CD% CALL ./../__Git.Scripts.Windows/__private/__git_check_unpushed_and_push.cmd %CD%
 
 PAUSE
