908f51b29bcf36b5ff0c200485111cda2c29c462
Konstantin Myrsaitov <myrsaitov@gmail.com>
2022-12-26 15:35:26 +0300

    Recursively Updated

--- ____git_pull.cmd
+++ ____git_pull.cmd
@@ -1,8 +1,8 @@
 @ECHO OFF
 :: Выполняет команду PULL для текущего репозитория
 
-:: Устанавливает заголовок окна
-TITLE Run "PULL [%CD%]"
+:: Заголовок окна - путь и имя файла
+TITLE %~0
 
 :: Выполняет команду PULL
 CALL ./../__Git.Scripts.Windows/__private/__git_CMD.cmd --PATH %CD% git pull
--- ____git_push.cmd
+++ ____git_push.cmd
@@ -1,8 +1,8 @@
 @ECHO OFF
 :: Выполняет команду PUSH для текущего репозитория
 
-:: Устанавливает заголовок окна
-TITLE Run "PUSH [%CD%]"
+:: Заголовок окна - путь и имя файла
+TITLE %~0
 
 :: Выполняет команду PUSH
 CALL ./../__Git.Scripts.Windows/__private/__git_CMD.cmd --PATH %CD% CALL ./../__Git.Scripts.Windows/__private/__git_check_unpushed_and_push.cmd
